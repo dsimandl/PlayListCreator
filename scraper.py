390cc258435eaad9a1061a7a9efc7aee42c913ad
David Simandl <david.simandl@gmail.com>
2015-03-10 11:20:50 -0400

    Created the music scraper class and made the page variables as flexible as I can

--- scraper.py
+++ scraper.py
@@ -1,16 +1,39 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
-url = "http://thealternateside.org/"
-page = request.urlopen("http://thealternateside.org/")
-soup_altside = BeautifulSoup(page)
-page.close()
+class MusicScraper:
 
+    def __init__(self, url):
+        self.url = url
+        self.page = ''
 
-top_albums_view = soup_altside.find("div", class_="view-top-albums")
-top_songs = top_albums_view.find("div", class_="field-content")
-top_song_list = top_songs.find_all("p")
+    def open_get_and_close_page(self):
+        self.page = request.urlopen(self.url)
+        soup_page = BeautifulSoup(self.page)
+        self.page.close()
+        return soup_page
 
-for top_song in top_song_list:
-    print(list(top_song)[1])
-    print(list(top_song)[3])
+    def soup_and_get_page_items(self, soup_method_and_vars_list, soup_page):
+        try:
+            while soup_method_and_vars_list:
+                if not soup_page:
+                    if isinstance(soup_method_and_vars_list[0][1], tuple):
+                        find_result = getattr(find_result, soup_method_and_vars_list[0][0])(str(
+                            soup_method_and_vars_list[0][1][0]), class_=soup_method_and_vars_list[0][1][1])
+                    else:
+                        find_result = getattr(find_result, soup_method_and_vars_list[0][0])(str(soup_method_and_vars_list[0][1]))
+                    soup_method_and_vars_list = soup_method_and_vars_list[1:]
+                else:
+                    if isinstance(soup_method_and_vars_list[0][1], tuple):
+                        find_result = getattr(soup_page, soup_method_and_vars_list[0][0])(
+                            soup_method_and_vars_list[0][1][0], class_=soup_method_and_vars_list[0][1][1])
+                    else:
+                        find_result = getattr(soup_page, soup_method_and_vars_list[0][0])(str(soup_method_and_vars_list[0][1]))
+                    soup_page = None
+                    soup_method_and_vars_list = soup_method_and_vars_list[1:]
+            print(find_result)
+            return find_result
+        except TypeError:
+            print('Invalid format for method and vars list')
+        except Exception:
+            print('Something went wrong! %s' % Exception)
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-import scraper
+from scraper import MusicScraper
 import unittest
 from http.client import HTTPResponse
 from bs4 import BeautifulSoup
@@ -6,11 +6,21 @@ from bs4 import BeautifulSoup
 
 class TestWebScraper(unittest.TestCase):
 
+    def setUp(self):
+        self.MusicScraper = MusicScraper('http://thealternateside.org/')
+
+
     def test_url_is_correct_and_is_working(self):
-        self.assertEqual(scraper.url, "http://thealternateside.org/")
-        self.assertIsInstance(scraper.page, HTTPResponse)
-        self.assertIsInstance(scraper.soup_altside, BeautifulSoup)
-        self.assertIn('<div class="field-content">', str(scraper.top_albums_view))
-        self.assertIn('<p>', str(scraper.top_songs))
-        self.assertNotEqual(scraper.top_song_list, [])
+        self.assertEqual(self.MusicScraper.url, "http://thealternateside.org/")
+
+        soup_page = self.MusicScraper.open_get_and_close_page()
+        self.assertIsInstance(self.MusicScraper.page, HTTPResponse)
+
+        self.assertIsInstance(soup_page, BeautifulSoup)
+
+        song_list = self.MusicScraper.soup_and_get_page_items([('find', ('div', 'view-top-albums')),
+                                                               ('find', ('div', 'field-content')), ('find_all', 'p')], soup_page)
+
+        self.assertIsInstance(song_list, list)
+
 
