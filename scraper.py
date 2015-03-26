21bc64c133b0209630283ac44da3ee2e1d8d1aca
David Simandl <david.simandl@gmail.com>
2015-03-26 18:41:31 -0400

    split the rdio playlist creation into its own file

--- scraper.py
+++ scraper.py
@@ -1,6 +1,5 @@
 from urllib import request
 from bs4 import BeautifulSoup
-from scraper_utils import ScraperUtils
 
 class MusicScraper:
 
@@ -44,60 +43,3 @@ class MusicScraper:
             print('Invalid format for method and vars list')
         except Exception:
             print('Something went wrong! %s' % str(Exception))
-
-class RdioAuthenticator:
-
-    def set_rdio_instance(self):
-        my_scraper_util = ScraperUtils()
-        my_rdio_instance = my_scraper_util.autheniticate_me()
-        return my_rdio_instance
-
-    def _call_rdio_api_util(self, rdio, method, params):
-        my_scraper_utils = ScraperUtils()
-        call_result = my_scraper_utils.rdio_api_call(rdio, method, params)
-        return call_result
-
-class TrackListCreator(RdioAuthenticator):
-
-    def __init__(self, result_list):
-        self.result_list = result_list
-
-    def check_artist_and_set_track_list(self, rdio_instance):
-        valid_track_string = ''
-        for result in self.result_list:
-            # just using the positions for now....
-            try:
-                artist_song_search_result = self._call_rdio_api_util(rdio_instance, 'search', {'query': "%s %s" % (list(result)[1].string,
-                                                                                   list(result)[3].string),
-                                                               'types': 'Artist, Song'})
-                if artist_song_search_result['result']['track_count'] == 0 \
-                    or artist_song_search_result['result']['track_count'] > 10:
-                    pass
-                else:
-                    valid_track_string += ''.join(artist_song_search_result['result']['results'][0]['key'] + ',')
-            except IndexError:
-                pass
-
-        return valid_track_string
-
-
-class PlaylistPopulator(RdioAuthenticator):
-
-    def __init__(self, playlist_name):
-        self.playlist_name = playlist_name
-
-    def get_playlist_keys(self, rdio_instance):
-        playlist_key = None
-        playlist_search_result = self._call_rdio_api_util(rdio_instance, 'getPlaylists', '')
-        for i in range(len(playlist_search_result['result']['owned'])):
-            if playlist_search_result['result']['owned'][i].get('name') == self.playlist_name:
-                    playlist_key = playlist_search_result['result']['owned'][i].get('key')
-        return playlist_key
-
-    def create_or_update_playlist(self, rdio_instance, valid_track_string, playlist_key, playlist_description):
-        if playlist_key:
-            self._call_rdio_api_util(rdio_instance, 'deletePlaylist', {'playlist': playlist_key})
-        self._call_rdio_api_util(rdio_instance, 'createPlaylist', {'name': self.playlist_name,
-                                                                 'description': playlist_description,
-                                                               'tracks': valid_track_string})
-        return 'Playlist updated!'
--- tests.py
+++ tests.py
@@ -3,7 +3,8 @@ import os
 import json
 from unittest.mock import Mock, patch
 
-from scraper import MusicScraper, TrackListCreator, PlaylistPopulator
+from scraper import MusicScraper
+from rdio_playlist_creator import TrackListCreator, PlaylistPopulator
 from bs4 import BeautifulSoup
 
 class TestWebScraper(unittest.TestCase):
@@ -29,7 +30,7 @@ class TestWebScraper(unittest.TestCase):
 
         self.assertIsInstance(song_list, list)
 
-    @patch('scraper.TrackListCreator._call_rdio_api_util')
+    @patch('rdio_playlist_creator.TrackListCreator._call_rdio_api_util')
     def test_check_artist_and_set_track_list(self, my_rdio_search):
         html = BeautifulSoup('<p>1.       <strong>Built To Spill</strong><br/>“Car”</p>')
         results = html.find_all('p')
@@ -38,28 +39,28 @@ class TestWebScraper(unittest.TestCase):
         valid_track_list = my_track_string_test.check_artist_and_set_track_list(self.rdio_instance)
         self.assertIsInstance(valid_track_list, str)
 
-    @patch('scraper.PlaylistPopulator._call_rdio_api_util')
+    @patch('rdio_playlist_creator.PlaylistPopulator._call_rdio_api_util')
     def test_get_playlist_keys(self, my_rdio_search):
         my_playlist_populator_test = PlaylistPopulator('jenny')
         my_rdio_search.return_value = json.loads(self.playlists_json)
         playlist_key_test = my_playlist_populator_test.get_playlist_keys(self.rdio_instance)
         self.assertTrue(playlist_key_test)
 
-    @patch('scraper.PlaylistPopulator._call_rdio_api_util')
+    @patch('rdio_playlist_creator.PlaylistPopulator._call_rdio_api_util')
     def test_update_playlist(self, my_rdio_search):
         my_playlist_populator_test_update = PlaylistPopulator('jenny')
         my_rdio_search.return_value = 'Success!'
         result = my_playlist_populator_test_update.create_or_update_playlist(self.rdio_instance, ['t123456'],
                                                                              't998899', 'My playlist for testing')
-        self.assertEqual(result, 'Playlist updated')
+        self.assertEqual(result, 'Playlist updated!')
 
-    @patch('scraper.PlaylistPopulator._call_rdio_api_util')
+    @patch('rdio_playlist_creator.PlaylistPopulator._call_rdio_api_util')
     def test_create_playlist(self, my_rdio_search):
         my_playlist_populator_test_create = PlaylistPopulator('TAS Top 20')
         my_playlist_populator_test_create.rdio = ''
         my_rdio_search.return_value = 'Success!'
         result = my_playlist_populator_test_create.create_or_update_playlist(self.rdio_instance, ['t123456'],
                                                                              None, 'My playlist for testing')
-        self.assertEqual(result, 'Playlist created')
+        self.assertEqual(result, 'Playlist updated!')
 
 
